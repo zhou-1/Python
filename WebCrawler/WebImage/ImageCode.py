5c7f3f72ed9b26b7c80d5ec7c2d8cf9746af8e46
Zhou <zhou.shen@marquette.edu>
2018-09-22 00:04:14 -0500

    Create ImageCode.py

---
+++ WebCrawler/WebImage/ImageCode.py
@@ -0,0 +1,23 @@
+#coding = utf-8
+import urllib
+import re
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    reg = 'src="(.+?\.jpg)" alt='
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, '%s.jpg' % x)
+        x+=1
+    return imglist
+
+html = getHtml("http://pic.yxdown.com/list/0_0_1.html")
+
+print getImg(html)
+
